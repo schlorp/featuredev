dcfa0c2e287b9b7260c070513473c905a5a1206f
schlorp <70897090+schlorp@users.noreply.github.com>
2021-09-24 14:31:23 +0200

    waypoint system

---
+++ Assets/scripts/Path.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Path : MonoBehaviour
+{
+   [SerializeField] private Waypoint[] _waypoints;
+            
+    public Waypoint GetPathStart()
+    {
+        return _waypoints[0];
+    }
+    public Waypoint GetPathEnd()
+    {
+        return _waypoints[_waypoints.Length - 1];
+    }
+    public Waypoint GetNextWaypoint(Waypoint currentWaypoint)
+    {
+        for (int i = 0; i < _waypoints.Length; i++)
+        {
+            if(_waypoints[i] == currentWaypoint)
+            {
+                return _waypoints[i + 1];
+            }
+        }
+        return null;
+    }
+
+   
+}
---
+++ Assets/scripts/PathFollower.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PathFollower : MonoBehaviour
+{
+    [SerializeField]private float _speed;
+    [SerializeField] private float _arrivalThreshold;
+    private Path _path;
+    private Waypoint _currentWaypoint;
+
+    private void Start()
+    {
+        SetupPath();
+    }
+
+    void SetupPath()
+    {
+        _path = FindObjectOfType<Path>();
+        _currentWaypoint = _path.GetPathStart();
+        transform.LookAt(_currentWaypoint.GetPosition());
+    }
+    private void Update()
+    {
+        
+        transform.Translate(Vector3.forward * _speed * Time.deltaTime);
+
+        float DistanceToWaypoint = Vector3.Distance(transform.position, _currentWaypoint.GetPosition());
+        Waypoint End = _path.GetPathEnd();
+
+        if(DistanceToWaypoint <= _arrivalThreshold)
+        {
+            if(_path.GetPathEnd() == _currentWaypoint)
+            {
+                PathComplete();
+            }
+            else
+            {
+                _currentWaypoint = _path.GetNextWaypoint(_currentWaypoint);
+                transform.LookAt(_currentWaypoint.GetPosition());
+            }
+        }
+
+        
+    }
+    void PathComplete()
+    {
+        Destroy(gameObject);
+    }
+}
